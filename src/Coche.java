0b0569dca012d2e61a4656dec850f58cc528263f
Gregory <gregbanegas.lovo@gmail.com>
2022-07-12 16:15:30 -0600

    primero

---
+++ src/Coche.java
@@ -0,0 +1,9 @@
+public class Coche {
+    String color;
+    public Coche(String color){
+        this.color = color;
+    }
+    public String getColor(){
+        return this.color;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    /**
+     * reloj analogo metodo segundos, minutos, hora
+     * auto rojo, 2 puertas, retroceso, girar
+     * paloma plumas, vuela, come
+     * velero flota navega
+     * yet alas propulsores volar, vuelo supersonico
+     **/
+
+    public static void main(String[] args) {
+
+        System.out.println("Hola Mundo");
+        System.out.println("Programacion II");
+
+        Coche miCoche = new Coche("Azul");
+        System.out.println(miCoche.getColor());
+    }
+}
